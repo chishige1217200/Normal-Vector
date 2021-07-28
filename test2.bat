16595691e123b7648e49517e8026d323d8b14b92
chishige1217200 <daichicchi01@gmail.com>
2021-07-28 22:14:50 +0900

    [add] testcase

---
+++ test2.bat
@@ -0,0 +1,6 @@
+python hosen.py < testcase7.txt
+python hosen.py < testcase8.txt
+python hosen.py < testcase9.txt
+python hosen.py < testcase10.txt
+python hosen.py < testcase11.txt
+python hosen.py < testcase12.txt
